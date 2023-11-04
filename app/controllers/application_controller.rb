a1b5c9e51432c848b74fe6ba73f18890c7524762
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-11-04 20:31:59 +0430

    Update  app/controllers/application_controller.rb   file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password)
+    end
+  end
 end
